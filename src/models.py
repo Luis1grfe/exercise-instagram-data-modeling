ffd29a0c8fd1a897a601878de6d38a368b19ad73
Luis1grfe <luismendezmor@outlook.com>
2021-03-21 22:15:13 +0000

    model

--- src/models.py
+++ src/models.py
@@ -26,6 +26,65 @@ class Address(Base):
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
+    website = Column(String(250), nullable=True)
+    presentation = Column(String(250), nullable=True)
+    photo = Column(String(250), nullable=False)
+
+class History(Base):
+    __tablename__ = 'history'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    media = Column(String(250), nullable=False)
+
+class Reel(Base):
+    __tablename__ = 'reel'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    media = photo = Column(String(250), nullable=False)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    media = Column(String(250), nullable=False)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    comment = Column(String(250), nullable=False)
+
+class Following(Base):
+    __tablename__ = 'following'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    follow_userid = Column(Integer, nullable=False)
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    follower_userid = Column(Integer, nullable=False)
+
+class Location(Base):
+    __tablename__ = 'location'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    country = Column(String(250), nullable=False)
+    city = Column(String(250), nullable=False)
+
+class Message(Base):
+    __tablename__ = 'message'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    other_userid = Column(Integer, nullable=False)
+    message = Column(String(500), nullable=False)
+
     def to_dict(self):
         return {}
 
